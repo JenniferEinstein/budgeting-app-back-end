9c2450976a5c355f6b66a9122e8b6fd8c8160bcd
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-09 16:36:59 -0400

    Add routes, including 404. Install cors. Add middleware (needed?)

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //DEPENDENCIES and CONFIGURATIONS
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
+// === MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
 
 //ROUTES
 app.get('/', (request, response) => {
@@ -8,10 +14,17 @@ app.get('/', (request, response) => {
 });
 
 
-app.get("/universe", (request, response) => {
-    response.send("Hello Universe!");
-  });
+app.get("/transactions", (request, response) => {
+    response.send("/transactions - The index page. Index/GET/Read and Create/POST/Create!");
+});
 
+app.get("/transactions/:id", (request, response) => {
+    response.send("/transactions:id - Show/GET/Read, Destroy/DELETE/Delete, and Update/PUT/Update)");
+});
+
+app.get("*", (request, response) => {
+    response.send("Oh, no! There is no page here. 404");
+});
 
 
 //EXPORT
