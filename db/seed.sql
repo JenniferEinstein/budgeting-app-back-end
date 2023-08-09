b0d4e4bf89a5e5122918dcc91c94a5c5c78fefd0
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-09 17:17:10 -0400

    Add info to seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c budgeting-app;
+
+INSERT INTO transacations (purchase_date, vendor, description, category, cost, payment_type, tax_related, notes)
+VALUES
+('5/5/2023', 'Verizon Wireless', 'monthly mobile phone bill', 'phone', 60.58, 'EFT', false),
+('5/13/2023', 'FastSpring', 'KanbanFlow', 'household', '5.45', 'paypal', false, 'Change payment to yearly instead?'),
+('5/18/23', 'Astound by RCN', 'internet and cable', 'internet', 125.58, 'cc Chase', false, 'Talk to BillSharks to see if they can get this lower.'),
+('5/19/23', 'Netflix', 'entertainment', 10.88, 'cc Chase', false),
+('6/2/23', 'ParkyKarkis Towers', 'electricity', 'utilities', 77.01, 'check', false, 'R reimbursed half?')
+
